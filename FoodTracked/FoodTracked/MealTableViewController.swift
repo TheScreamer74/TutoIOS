eb84fa0911c28d41cab5cbbff1ab994e8f3c69e4
Thomas MLD <thuyghues@myluckyday.fr>
2020-06-22 12:44:06 +0200

    added a table view for the selection

---
+++ FoodTracked/FoodTracked/MealTableViewController.swift
@@ -0,0 +1,124 @@
+//
+//  MealTableViewController.swift
+//  FoodTracked
+//
+//  Created by Thomas on 22/06/2020.
+//  Copyright © 2020 Thomas. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewController: UITableViewController {
+
+    //MARK: Properties
+    
+    var meals = [Meal]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Load the sample data.
+        loadSimpleMeals()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //Table vew cells are reused and should be dequeued using a cell indentifer.
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    //MARK: Private Methods
+    
+    private func loadSimpleMeals() {
+        
+        let burgerImg = UIImage(named: "Burger")
+        let pizzaImg = UIImage(named: "Pizza")
+        let pouletImg = UIImage(named: "Poulet")
+        
+        guard let mealBurger = Meal(name: "Burger", photo: burgerImg, rating: 5) else {
+            fatalError("Unable to instantiate mealBurger")
+        }
+        
+        guard let mealPizza = Meal(name: "Pizza", photo: pizzaImg, rating: 4) else {
+            fatalError("Unable to instantiate mealPizza")
+        }
+        
+        guard let mealPoulet = Meal(name: "Poulet", photo: pouletImg, rating: 3) else {
+            fatalError("Unable to instantiate mealPoulet")
+        }
+        
+        meals += [mealBurger, mealPizza, mealPoulet]
+        
+
+    }
+}
---
+++ FoodTracked/FoodTracked/MealViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MealViewController.swift
+//  FoodTracked
+//
+//  Created by Thomas on 19/06/2020.
+//  Copyright © 2020 Thomas. All rights reserved.
+//
+
+import UIKit
+
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameTextField.delegate = self
+    }
+
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Action
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+}
